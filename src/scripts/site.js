c5619de643b2681d69c0e828eeff1df99062688a
Dienul Paramarta <dienul.paramarta@gmail.com>
2017-04-20 17:53:09 -0500

    loader change

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -32,22 +32,24 @@ function navigateToPage(pageUrl, onFinish){
     var $nav = $('#onePageNav');
     var $mainPane = $('main');
     
-    $nav.addClass('loader');
+    var $menuButton = $('#onePageNavMenuBtn');
+    if ($menuButton.is(':visible')){
+        $('#onePageNavPagesList').addClass('hidden-xs-down');
+        $menuButton.removeClass('pushed');
+    }
 
+    $nav.addClass('loader');
+    $mainPane.html('');
     $.get(pageUrl, function(data){
         $('main').html(data);
         
-        var $menuButton = $('#onePageNavMenuBtn');
-        if ($menuButton.is(':visible')){
-            $('#onePageNavPagesList').addClass('hidden-xs-down');
-            $menuButton.removeClass('pushed');
+        if (typeof onFinish === "function"){
+            onFinish();
         }
+
         $nav.removeClass('loader');
     });
 
-    if (typeof onFinish === "function"){
-        onFinish();
-    }
 }
 
 $('main').on('click', '#displayLoaderBtn', function(e){
